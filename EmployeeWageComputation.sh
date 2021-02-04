0bee1a37463e6502875ce8b4a13c9379b61ae931
bhimineni-kalyani <bhiminenikalyani@gmail.com>
2021-02-04 23:37:14 +1400

    Refctor - DailyPayment functonality added

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -3,10 +3,16 @@
 echo "Welcome to Employee Wage Computation Program"
 
 isPresent=1;
+WageperHour=20;
+FullDayHour=8;
+DailyPayment=0;
 checkRandom=$((RANDOM%2))
 if [ $isPresent -eq $checkRandom ]
 then
      echo "Employee is Present"
+     DailyPayment=$(($WageperHour*$FullDayHour))
+     echo "daily Payment is: " $DailyPayment
 else 
      echo "Employee is Absent"
+     echo "daily Payment is: " $DailyPayment
 fi
