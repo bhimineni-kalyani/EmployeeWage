638a0bc09c2aaab0521cdf3092f9b2036f703727
bhimineni-kalyani <bhiminenikalyani@gmail.com>
2021-02-05 11:54:43 +1400

    Refactor - ParttimeEmployeeandWage functionlity added

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -6,13 +6,22 @@ isPresent=1;
 WageperHour=20;
 FullDayHour=8;
 DailyPayment=0;
+ParttimeEmployee=8;
 checkRandom=$((RANDOM%2))
-if [ $isPresent -eq $checkRandom ]
+if [ $FullDayHour -eq $checkRandom ]
 then
      echo "Employee is Present"
      DailyPayment=$(($WageperHour*$FullDayHour))
      echo "daily Payment is: " $DailyPayment
-else 
+     EmployeeHours=8;
+elif [ $ParttimeEmployee -eq $checkRandom ] 
+then
      echo "Employee is Absent"
      echo "daily Payment is: " $DailyPayment
+     EmployeeHours=4;
+else
+     EmployeeHours=0;
 fi
+
+salary=$(($EmployeeHours*$WageperHour));
+
