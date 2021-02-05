9abd4a853d7486dec273218514e268d290be0a00
bhimineni-kalyani <bhiminenikalyani@gmail.com>
2021-02-05 16:03:58 +1400

    Refactor - WagesforaMonth functionality added

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -3,25 +3,26 @@
 echo "Welcome to Employee Wage Computation Program"
 
 isPresent=1;
-WageperHour=20;
-FullDayHour=8;
-DailyPayment=0;
-ParttimeEmployee=8;
-checkRandom=$((RANDOM%2))
-if [ $FullDayHour -eq $checkRandom ]
-then
-     echo "Employee is Present"
-     DailyPayment=$(($WageperHour*$FullDayHour))
-     echo "daily Payment is: " $DailyPayment
-     EmployeeHours=8;
-elif [ $ParttimeEmployee -eq $checkRandom ] 
-then
-     echo "Employee is Absent"
-     echo "daily Payment is: " $DailyPayment
-     EmployeeHours=4;
-else
-     EmployeeHours=0;
-fi
+EmployeeHour=20;
+TotalSalary=0;
+ParttimeEmployee=2;
+NumberWorkingDays=20;
 
-salary=$(($EmployeeHours*$WageperHour));
+for (( day=1; day<=$NumberWorkingDays; day++ ))
+do
+    checkRandom=$((RANDOM%3));
+       case $checkRandom in 
+           $FullDayHour)
+                EmployeeHours=8
+                ;;
+           $ParttimeEmployee) 
+                EmployeeHours=4
+                ;;
+           *)
+                EmployeeHours=0
+                ;;
+       esac
 
+       salary=$(($EmployeeHours*$EmployeeHour));
+       TotalSalary=$(($TotalSalary+$salary));
+done
